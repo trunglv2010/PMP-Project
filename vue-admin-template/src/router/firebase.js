c5adb01bd6d1cd798f4046a2685e743c747efc5c
le van trung <trunglv2010@gmail.com>
2019-05-22 17:29:09 +0700

    commit code vue-admin-template

---
+++ vue-admin-template/src/router/firebase.js
@@ -0,0 +1,18 @@
+//<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyDtrhL75_b-OVXNy4T0VV85GHNFNBXXKq4",
+authDomain: "my-project-1537738789948.firebaseapp.com",
+databaseURL: "https://my-project-1537738789948.firebaseio.com",
+projectId: "my-project-1537738789948",
+storageBucket: "my-project-1537738789948.appspot.com",
+messagingSenderId: "858272398593",
+appId: "1:858272398593:web:bcc16f0e158b7012"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
